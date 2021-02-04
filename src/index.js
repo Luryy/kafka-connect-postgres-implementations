f6cfd4509cca0093648ff290ac016ed7f1e9046d
Lucas Yuri <lucasyuri56@hotmail.com>
2021-02-04 12:56:51 -0300

    feat: add kafka consumer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 const kafka = require('./kafka-config');
 
-console.log(process.env.HOST)
+(async () => {
+
+    const consumer = kafka.consumer({
+        groupId: 'KafkaConsumer',
+    });
+
+    await consumer.connect();
+    await consumer.subscribe({
+      topic: `${process.env.KAFKA_SERVER_NAME}.public.user_info`, // ${database.server.name}.public.${table-name}
+    });
+
+    await consumer.run({
+      async eachMessage({ message }) {
+        if (message.value !== null) {
+          const { payload } = JSON.parse(message.value.toString());
+          console.log(payload);
+        }
+      },
+    });
+
+    console.log('Running Kafka Consumer!');
+})();
